6cecd23f8fdec903cd26f1afcd87be6c74ee5b69
Kirill Shvetsov <kirillshveetsov@gmail.com>
2023-10-21 20:53:23 +1200

    Второе задание

--- task-2/bin/task_2.dart
+++ task-2/bin/task_2.dart
@@ -7,18 +7,16 @@ class Engine {
       {required this.engineCapacity, required this.power, required this.name});
 
   @override
-  String toString(){
-    String adoutMe = 'название: $name, мощность: $power, объем двигателя: $engineCapacity';
-    return adoutMe;
+  String toString() {
+    return 'название: $name, мощность: $power, объем двигателя: $engineCapacity';
   }
 }
 
 class Car {
-  static const String _brand =
-      'Subaru'; // Бренд (один бренд у всех автомобилей)
+  static const String brand = 'Subaru'; // Бренд (один бренд у всех автомобилей)
   final String model; // Модель
-  String color; // Цвет (можно перекрасить)
   final DateTime yearOfRelease; // Год выпуска
+  String color; // Цвет (можно перекрасить)
   Engine engine; // Двигатель
   bool? spareWheel; // Запасное колесо
 
@@ -27,7 +25,8 @@ class Car {
       {required this.model,
       required this.yearOfRelease,
       required this.engine,
-      required this.color});
+      required this.color,
+      this.spareWheel = false});
 
   // Полная комплектация
   Car.fullEquipment(
@@ -38,8 +37,6 @@ class Car {
     spareWheel = true;
   }
 
-  String get brand => _brand;
-
   void drive() {
     print('Машина поехала');
   }
@@ -49,7 +46,7 @@ class Car {
   }
 
   @override
-  String toString(){
+  String toString() {
     String adoutMe = '''
     Бренд: $brand
     Модель: $model
@@ -66,10 +63,26 @@ void main() {
   final engA310 = Engine(engineCapacity: 1500, power: 300, name: 'A310');
   final engA410 = Engine(engineCapacity: 2000, power: 400, name: 'A410');
 
-  final car1 = Car(model: 'vm1', yearOfRelease: DateTime.utc(2001,01,1), engine: engA210, color: 'White');
-  final car2 = Car(model: 'vm2', yearOfRelease: DateTime.utc(2004,01,1), engine: engA310, color: 'Black');
-  final car3 = Car(model: 'vm3', yearOfRelease: DateTime.utc(2007,01,1), engine: engA410, color: 'White');
-  final car4 = Car.fullEquipment(model: 'vm3', yearOfRelease: DateTime.utc(20010,01,1), engine: engA410, color: 'White');
+  final car1 = Car(
+      model: 'vm1',
+      yearOfRelease: DateTime.utc(2001, 01, 1),
+      engine: engA210,
+      color: 'White');
+  final car2 = Car(
+      model: 'vm2',
+      yearOfRelease: DateTime.utc(2004, 01, 1),
+      engine: engA310,
+      color: 'Black');
+  final car3 = Car(
+      model: 'vm3',
+      yearOfRelease: DateTime.utc(2007, 01, 1),
+      engine: engA410,
+      color: 'White');
+  final car4 = Car.fullEquipment(
+      model: 'vm3',
+      yearOfRelease: DateTime.utc(20010, 01, 1),
+      engine: engA410,
+      color: 'White');
 
   print(car1);
   print(car2);
