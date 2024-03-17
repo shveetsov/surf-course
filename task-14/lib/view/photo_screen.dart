9a0b85bf8610efc830dde4800ad0cb628b9160b4
Kirill Shvetsov <kirillshveetsov@gmail.com>
2024-03-17 13:03:12 +1200

    Задание — Галерея фотографий

--- task-14/lib/view/photo_screen.dart
+++ task-14/lib/view/photo_screen.dart
@@ -16,24 +16,15 @@ class PhotoScreen extends StatefulWidget {
 class _PhotoScreenState extends State<PhotoScreen> {
   static const _defaultAnimDuration = Duration(milliseconds: 300);
 
-  // текущая картинка
   late var currentPage = widget.index;
 
   late final _pageController = PageController(
-    /// Начальным элементом будет нажатая картинка
     initialPage: currentPage,
-
-    /// Отвечает за то, сколько пространства PageView занимает активный элемент.
-    /// По умолчанию равно 1, что означает, что элемент занимает все пространство.
-    ///
-    /// Выставив такое значение, мы сможем увидеть соседние элементы.
     viewportFraction: 0.8,
   );
 
   @override
   void initState() {
-    /// С этого момента при любом изменении, произошедшем в PageController,
-    /// будет вызвать метод _onPageChanged
     _pageController.addListener(_onPageChanged);
     super.initState();
   }
@@ -46,15 +37,11 @@ class _PhotoScreenState extends State<PhotoScreen> {
     super.dispose();
   }
 
-  /// Здесь мы будем реагировать на изменение текущего положения в рамках PageView
   void _onPageChanged() {
-    /// Запоминаем последнюю активную страницу
     final prevPage = currentPage;
 
-    /// Вычисляем текущую (или, вернее, вычисляем страницу, к которой мы ближе всего)
     currentPage = _pageController.page?.round() ?? currentPage;
 
-    /// Если наша страница изменилась, делаем setState, чтобы вызвать перестройку виджета
     if (prevPage != currentPage) setState(() {});
   }
 
@@ -81,19 +68,12 @@ class _PhotoScreenState extends State<PhotoScreen> {
           controller: _pageController,
           itemCount: widget.listPhotos.length,
           itemBuilder: (_, i) => Center(
-            /// Используем виджет неявной анимации, чтобы достичь
-            /// красивого эффекта плавного масштабирования
             child: AnimatedScale(
               duration: const Duration(milliseconds: 300),
-
-              /// Если страница является текущей, её масштаб будет оригинальным (1),
-              /// иначе - чуть меньше
               scale: currentPage == i ? 1 : 0.85,
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 0),
                 child: GestureDetector(
-                  /// Теперь при нажатии на определенный элемент мы с помощью
-                  /// PageController перемещаться к нему
                   onTap: () => _pageController.animateToPage(
                     i,
                     duration: _defaultAnimDuration,
@@ -120,10 +100,9 @@ class _PageViewItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      // Соотношение сторон 1:2
       aspectRatio: 1 / 2,
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(24.0), // Радиус закругления
+        borderRadius: BorderRadius.circular(24.0),
         child: Image.asset(
           photo.imagePath,
           fit: BoxFit.cover,
