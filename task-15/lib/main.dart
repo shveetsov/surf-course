2a13375b5658461e4f783769ad127dda01fd837d
Kirill Shvetsov <kirillshveetsov@gmail.com>
2024-03-20 10:43:40 +1200

    Задание — Магический шар

--- task-15/lib/main.dart
+++ task-15/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shake/shake.dart';
 import 'package:task_15/app_constants.dart';
+import 'package:task_15/dots_loadig_indicator.dart';
 
 import 'models/magic_ball/magic_ball_bloc.dart';
 
@@ -165,60 +166,5 @@ class _MainScreenState extends State<MainScreen> {
         ),
       ),
     );
-  }
-}
-
-class DotsLoadingIndicator extends StatefulWidget {
-  const DotsLoadingIndicator({super.key});
-
-  @override
-  State<DotsLoadingIndicator> createState() => _DotsLoadingIndicatorState();
-}
-
-class _DotsLoadingIndicatorState extends State<DotsLoadingIndicator> with TickerProviderStateMixin {
-  late final AnimationController _animationController = AnimationController(
-    duration: const Duration(milliseconds: 800),
-    vsync: this,
-  )..repeat();
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: List.generate(3, (index) => buildDot(index)),
-    );
-  }
-
-  Widget buildDot(int index) {
-    return AnimatedBuilder(
-      animation: _animationController,
-      builder: (context, child) {
-        final scale = 1.0 + 0.5 * (calculateValue(index) <= 0.5 ? calculateValue(index) : 1 - calculateValue(index));
-        return Transform.scale(
-          scale: scale,
-          alignment: Alignment.center,
-          child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 4.0),
-            height: 8,
-            width: 8,
-            decoration: const BoxDecoration(
-              color: AppColors.white,
-              shape: BoxShape.circle,
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  double calculateValue(int index) {
-    final value = (_animationController.value - index / 3).abs() % 1;
-    return value;
   }
 }
